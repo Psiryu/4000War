81c01c217b307f10a50df1b976b06605a67f96af
jasonenglehart <tcpsolution@gmail.com>
2014-02-15 18:23:53 -0500

    Player Class Updated
    Added Constructors (incomplete) and GETS SETS (incomplete)

--- War/src/Player.java
+++ War/src/Player.java
@@ -3,19 +3,63 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.util.ArrayList;
 /**
  *
  * @author Fearless Jay
  */
 public class Player 
 {
+        
     
-        Node Capital;
-        CombatUnit[] listOfCombatUnits;
+    
+        Node capital;
+        ArrayList CombatUnit[];
         int politicalPower;
         boolean isComp;
         int  playerID;
+        
+        
+        /*Constructor*/
+        public Player(boolean _isComp,Node _capital, int id)
+        {
+            playerID = id;
+            politicalPower = 100;
+            isComp = _isComp;
+            capital = _capital;
+            /*NOW We place the COMBATS UNIT ACCORDING TO SCENARIO SPECIFICATIONS*/
+            
+        }
+        
+        
+        
+        /*GETS AND SETS*/
+        public int getPolitcalPower()
+        {
+            return politicalPower;
+        }
+        public void setPoliticalPower(int x)
+        {
+            politicalPower = x;
+        }
+        
+        public boolean getIsComp()
+        {
+            return isComp;
+        }
+        public void setIsComp(boolean x)
+        {
+            isComp = x;
+        }
+        
+        public CombatUnit[] getlistOfCombatUnits()
+        {
+            return listOfCombatUnits;
+        }
+        public void listOfCombatUnits(CombatUnit[] x)
+        {
+            listOfCombatUnits = x;
+        }
         
         
     
