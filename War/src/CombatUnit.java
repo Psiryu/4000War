c5e484b240249396dcd16135c3512a55fd85c0a1
jasonenglehart <tcpsolution@gmail.com>
2014-02-15 19:04:02 -0500

    Updated Combat Unit
    Not 100% done

--- War/src/CombatUnit.java
+++ War/src/CombatUnit.java
@@ -8,15 +8,28 @@
  *
  * @author Fearless Jay
  */
-public class CombatUnit extends Player
+public class CombatUnit
 {
     int cUnitID;
+    int playerID;
     int size;
     int healthRating; /*OUT OF XX*/
     int distanceFromCapital; /*Possible can FUNCTION*/
     Node location;
     
+    public CombatUnit(int _cUnitID, int _size, int _healthRating, int distance, int _playerID)
+    {
+        cUnitID = _cUnitID;
+        playerID = _playerID;
+        size = _size;
+        healthRating = _healthRating;
+        distanceFromCapital = distance;    /*probably have to get distance from node class*/
+        
+        
+        /**/
+        
     
+    }
     
     int GetBattleStrengh()
     {
