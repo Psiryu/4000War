9145918e37d9511f790c8a3a1906def012a47ce1
BotBen <ben.yntema@gmail.com>
2014-03-01 18:37:40 -0500

    Map Update - Change Maps -> Scenario
    Re-purposed Maps.java to be used in the sense of Scenarios. Added
    functionality to create a foundation for the backend of the map.

---
+++ War/src/Scenario.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author BotBen
+ */
+public class Scenario 
+{
+    Node[] listOfNodes;
+    Road[] listOfRoads;
+    int scenarioID = 0;
+    
+    void Initialize(){
+        switch (scenarioID) {
+            case 0:
+                String[] names = {"Utica","Fidenza","Faenza","Volterra"
+                        ,"Saturnia","Senigalia","Clusium","Palestrina","Naples"
+                        ,"Nola","Brindis","Dyrrhachulum","Kavala","Roma"};
+                int[][] capitalDistances = {{1,5},{3,6},{2,5},{2,5},{2,5},{1,4}
+                        ,{1,4},{1,4},{1,2},{2,1},{3,0},{4,1},{5,2},{0,3}};
+                listOfNodes = new Node[14];
+                for (int i = 0;i < 12; i++){
+                    listOfNodes[i].name = names[i];
+                    listOfNodes[i].distanceFromCapitalBlue = capitalDistances[i][0];
+                    listOfNodes[i].distanceFromCapitalRed = capitalDistances[i][1];
+                    if (names[i] == "Roma")
+                        listOfNodes[i].isCapitalBlue = true;
+                    else if (names[i] == "Brindis")
+                        listOfNodes[i].isCapitalRed = true;
+                }
+                
+        }
+    }
+    
+}
