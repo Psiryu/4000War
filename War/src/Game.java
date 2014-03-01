2c065f9bfb1ba23ade2e44abf1c8badaec17738f
BotBen <ben.yntema@gmail.com>
2014-03-01 15:49:12 -0500

    Weather happens
    A weather variable has been added to Global with get and set in Game
    class.

--- War/src/Game.java
+++ War/src/Game.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,5 +15,16 @@ public class Game
 {
     
     public static int turnCount = 0 ; 
+    
+    int getWeather()
+    {
+        return Global.weather;
+    }
+    
+    void updateWeather()
+    {
+        Random random = new Random();
+        Global.weather = (int) (5 * random.nextDouble());
+    }
     
 }
