2918a7c2854c2fd756d54c9af42b6207a38df8df
BotBen <ben.yntema@gmail.com>
2014-03-13 15:15:59 -0400

    MapEvent now supports movement
    Title says it all.
    All non-combating units will have the locations adjusted.

--- War/src/MapEvent.java
+++ War/src/MapEvent.java
@@ -18,8 +18,10 @@ public class MapEvent {
     private ArrayList<Road> blueUnitRoad; // list of roads occupied by blue faction
     private ArrayList<Node> redUnitEnd; // list of node end locations for red
     private ArrayList<Node> blueUnitEnd; // list of node end locations for blue
-    private ArrayList<CombatUnit> redCombatList;
-    private ArrayList<CombatUnit> blueCombatList;
+    private ArrayList<CombatUnit> redCombatListCollision;
+    private ArrayList<CombatUnit> blueCombatListCollision;
+    private ArrayList<CombatUnit> redCombatListNode;
+    private ArrayList<CombatUnit> blueCombatListNode;
 
     /*
      int[i][j] armies
@@ -39,8 +41,10 @@ public class MapEvent {
         blueUnitRoad = new ArrayList<Road>();
         redUnitEnd = new ArrayList<Node>();
         blueUnitEnd = new ArrayList<Node>();
-        redCombatList = new ArrayList<CombatUnit>();
-        blueCombatList = new ArrayList<CombatUnit>();
+        redCombatListCollision = new ArrayList<CombatUnit>();
+        blueCombatListCollision = new ArrayList<CombatUnit>();
+        redCombatListNode = new ArrayList<CombatUnit>();
+        blueCombatListNode = new ArrayList<CombatUnit>();
     }
 
     // Method called to to add a movement to the registry
@@ -59,48 +63,81 @@ public class MapEvent {
     // Method called to in order to simulate simutanious movement
     public void processEvents() {
         for (int i = 0; i < Scenario.listOfRoads.length; i++) {
-            for (int j = 0; j < redCombatList.size(); j++) {
+            for (int j = 0; j < combatUnitsRed.size(); j++) {
                 if (redUnitRoad.get(i).roadID == i) {
-                    redCombatList.add(combatUnitsRed.get(i));
+                    redCombatListCollision.add(combatUnitsRed.get(i));
                 }
             }
-            for (int j = 0; j < blueCombatList.size(); j++) {
+            for (int j = 0; j < combatUnitsBlue.size(); j++) {
                 if (blueUnitRoad.get(i).roadID == i) {
-                    blueCombatList.add(combatUnitsBlue.get(i));
+                    blueCombatListCollision.add(combatUnitsBlue.get(i));
                 }
             }
-            if (redCombatList.size() > 0 && blueCombatList.size() > 0) {
+            if (redCombatListCollision.size() > 0 && blueCombatListCollision.size() > 0) {
                 /*
-                There was a collision battle triggered on this road
-                the two arrays redCombatList and blueCombatList contain those involved
-                */
+                 There was a collision battle triggered on this road
+                 the two arrays redCombatList and blueCombatList contain those involved
+                 */
             }
         }
         for (int i = 0; i < Scenario.listOfNodes.length; i++) {
             for (int j = 0; j < redUnitEnd.size(); j++) {
                 if (redUnitEnd.get(i).id == i) {
-                    redCombatList.add(combatUnitsRed.get(i));
+                    redCombatListNode.add(combatUnitsRed.get(i));
                 }
             }
             for (int j = 0; j < blueUnitEnd.size(); j++) {
                 if (blueUnitEnd.get(i).id == i) {
-                    blueCombatList.add(combatUnitsBlue.get(i));
+                    blueCombatListNode.add(combatUnitsBlue.get(i));
                 }
             }
-            if (redCombatList.size() > 0 && blueCombatList.size() > 0) {
+            if (redCombatListNode.size() > 0 && blueCombatListNode.size() > 0) {
                 /*
-                There was a  battle triggered on this node
-                the two arrays redCombatList and blueCombatList contain those involved
-                */
+                 There was a  battle triggered on this node
+                 the two arrays redCombatList and blueCombatList contain those involved
+                 */
+            }
+        }
+        for (int j = 0; j < combatUnitsRed.size(); j++) {
+            if (!redCombatListCollision.contains(combatUnitsRed.get(j)) && !redCombatListNode.contains(combatUnitsRed.get(j))){
+                moveUnit(combatUnitsRed.get(j), redUnitEnd.get(j));
+            }
+        }
+        for (int j = 0; j < combatUnitsBlue.size(); j++) {
+            if (!blueCombatListCollision.contains(combatUnitsBlue.get(j)) && !blueCombatListNode.contains(combatUnitsBlue.get(j))){
+                moveUnit(combatUnitsBlue.get(j), blueUnitEnd.get(j));
             }
         }
     }
 
+    private void moveUnit(CombatUnit unit, Node endLocation){
+        unit.previousLocation = unit.location;
+        unit.location = endLocation;
+    }
+    
     // Method called to in order to handle unit merges
-    public void mergeUnits() {
+    public void mergeUnits(CombatUnit one, CombatUnit two) {
+        int[] scaled = {0, 0, 0};
+        int sumSize = one.size + two.size;
+        int[] sizes = {one.size, two.size, sumSize};
+
+        for (int i = 0; i < 3; i++) {
+            if (sizes[i] < 6) {
+                scaled[i] = 0;
+            } else if (sizes[i] < 11) {
+                scaled[i] = 1;
+            } else {
+                scaled[i] = 2;
+            }
+        }
+
         /*
-        scale the backend army size to account for variance in unit size ranges
-        */
+         scale the backend army size to account for variance in unit size ranges
+         */
+    }
+
+    public void divideUnits(CombatUnit unit) {
+
     }
 
     // Method to reset the arrays at turn's end
@@ -111,7 +148,9 @@ public class MapEvent {
         blueUnitRoad = new ArrayList<Road>();
         redUnitEnd = new ArrayList<Node>();
         blueUnitEnd = new ArrayList<Node>();
-        redCombatList = new ArrayList<CombatUnit>();
-        blueCombatList = new ArrayList<CombatUnit>();
+        redCombatListCollision = new ArrayList<CombatUnit>();
+        blueCombatListCollision = new ArrayList<CombatUnit>();
+        redCombatListNode = new ArrayList<CombatUnit>();
+        blueCombatListNode = new ArrayList<CombatUnit>();
     }
 }
