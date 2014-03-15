2aefeb9f8430e397bd7c953b895bc07f6e0a0d17
BotBen <ben.yntema@gmail.com>
2014-03-15 18:06:33 -0400

    Rumour Update
    Functionality added to calculate the overall fog value for a player and
    to obtain the pure intelligence values for each node.

--- War/src/Rumour.java
+++ War/src/Rumour.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /*
@@ -6,7 +7,6 @@ import java.util.Random;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Ben
@@ -14,83 +14,140 @@ import java.util.Random;
 public class Rumour {
 
     public static Node[] listOfNodes; // storage of available location
-    public static CombatUnit[] listOfUnitsRed; // storage of current red units
-    public static CombatUnit[] listOfUnitsBlue; // storage of current blue units
+    public static ArrayList<CombatUnit> listOfUnitsRed; // storage of current red units
+    public static ArrayList<CombatUnit> listOfUnitsBlue; // storage of current blue units
     public static Random random = new Random(); // establish a random variable
-    
+
     // Constructor of the rumour system
-    public Rumour(Node[] _listOfNodes, CombatUnit[] _listOfUnitsRed, CombatUnit[] _listOfUnitsBlue) {
+    public Rumour() {
         // set the necessary parameters
-        listOfNodes = _listOfNodes;
-        listOfUnitsRed = _listOfUnitsRed;
-        listOfUnitsBlue = _listOfUnitsBlue;
+        listOfNodes = Scenario.listOfNodes;
+        listOfUnitsRed = new ArrayList<CombatUnit>();
+        listOfUnitsBlue = new ArrayList<CombatUnit>();
     }
 
     // Method to update the rumour awareness of current units
-    public void updateUnitRegistry(CombatUnit[] _listOfUnitsRed, CombatUnit[] _listOfUnitsBlue) {
+    public void updateUnitRegistry() {
         // set the new unit lists
-        listOfUnitsRed = _listOfUnitsRed;
-        listOfUnitsBlue = _listOfUnitsBlue;
+        listOfUnitsRed = Scenario.redPlayer.combatUnits;
+        listOfUnitsBlue = Scenario.bluePlayer.combatUnits;
+    }
+
+    public void playerFogValueUpdate() {
+        Player currentPlayer;
+        Node currentNode;
+        int roadFogSum = 0;
+        int nodeFogSum = 0;
+        int fogSum;
+        int randomFog;
+
+        // determine the player to be used
+        if (Scenario.redPlayer.playerID == Global.curPlayer) {
+            currentPlayer = Scenario.redPlayer;
+        } else {
+            currentPlayer = Scenario.bluePlayer;
+        }
+
+        for (int i = 0; i < currentPlayer.combatUnits.size(); i++) {
+            currentNode = currentPlayer.combatUnits.get(i).location;
+            
+            for (int j = 0; j < Scenario.listOfRoads.length; j++) {
+                if (Scenario.listOfRoads[j].locationA.id == currentNode.id || Scenario.listOfRoads[j].locationB.id == currentNode.id) {
+                    roadFogSum += Scenario.listOfRoads[j].fogValue;
+                }
+            }
+            
+            nodeFogSum += currentNode.fogValue;
+        }
+
+        randomFog = -100 + (int) (random.nextDouble() * 200);
+        fogSum = nodeFogSum + roadFogSum + currentPlayer.politicalPower + randomFog;
+    }
+
+    public void reportRumour(int nodeID) {
+        ArrayList<Integer> units = nodeOccupancy(nodeID);
+        
     }
     
+    private ArrayList<Integer> nodeOccupancy (int nodeID){
+        Node currentNode = Scenario.listOfNodes[nodeID];
+        ArrayList<Integer> units = new ArrayList<Integer>();
+        Player currentOpponent;
+        
+        // determine the player to be used
+        if (Scenario.redPlayer.playerID == Global.curPlayer) {
+            currentOpponent = Scenario.bluePlayer;
+        } else {
+            currentOpponent = Scenario.redPlayer;
+        }
+        
+        for (int i = 0; i < currentOpponent.combatUnits.size(); i++){
+            if (currentOpponent.combatUnits.get(i).location.id == currentNode.id){
+                units.add(currentOpponent.combatUnits.get(i).size);
+                units.add(currentOpponent.combatUnits.get(i).timeStationary);
+            }
+        }
+        
+        return units;
+    }
     /*
-    From here we need to establish how to translate our fog functions
-    FogOfWar = [(L+[surz])*numA[i]]+PoliticalPower+random
-    where: L = location value {0..10}
-        surz = surrounding path values {0..4}
-        PoliticalPower {0..30}
-        random {-100..100}
-        0 < FogOfWar < 100
+     From here we need to establish how to translate our fog functions
+     FogOfWar = [(L+[surz])*numA[i]]+PoliticalPower+random
+     where: L = location value {0..10}
+     surz = surrounding path values {0..4}
+     PoliticalPower {0..30}
+     random {-100..100}
+     0 < FogOfWar < 100
     
      public void ArmiesHere(int[][] armies)
-    {
-        String sizes = "";
-        int i = 0;
-        for (int[] armie : armies) {
-            if (nodeSelected == armies[i][2])
-                sizes += (ConvertSize(armies[i][1], armies[i][3]) + " ");
-            i++;
-        }
+     {
+     String sizes = "";
+     int i = 0;
+     for (int[] armie : armies) {
+     if (nodeSelected == armies[i][2])
+     sizes += (ConvertSize(armies[i][1], armies[i][3]) + " ");
+     i++;
+     }
         
-        if(sizes.equals(""))
-            sizes = "none";
+     if(sizes.equals(""))
+     sizes = "none";
         
-        labelInfo5.setText("Your armies here: " + sizes);
+     labelInfo5.setText("Your armies here: " + sizes);
         
-        //popupMenu.add(null)
-    }
+     //popupMenu.add(null)
+     }
     
-    public int[][] ObtainArmies(int[][] listArmy) {
-        //creates list to use to keep track of armies.
-        //first portion of rectangular array represents the differnt armies
-        //within the list. Second portion is [id, size, location].  
+     public int[][] ObtainArmies(int[][] listArmy) {
+     //creates list to use to keep track of armies.
+     //first portion of rectangular array represents the differnt armies
+     //within the list. Second portion is [id, size, location].  
 
-        if(curPlayer == 0)
-        {
-            listArmy = new int[Scenario.redPlayer.combatUnits.size()][4];
-            for(int i = 0; i < Scenario.redPlayer.combatUnits.size(); i++) {
-                listArmy[i][0] = Scenario.redPlayer.combatUnits.get(i).cUnitID;
-                listArmy[i][1] = Scenario.redPlayer.combatUnits.get(i).size;
-                listArmy[i][2] = Scenario.redPlayer.combatUnits.get(i).GetLocation().id;
-                if (Scenario.redPlayer.combatUnits.get(i).isFleet == true)
-                    listArmy[i][3] = 1;
-                else
-                    listArmy[i][3] = 0;
-            }
-        } else {
-            listArmy = new int[Scenario.bluePlayer.combatUnits.size()][4];
-            for(int i = 0; i < Scenario.bluePlayer.combatUnits.size(); i++) {
-                listArmy[i][0] = Scenario.bluePlayer.combatUnits.get(i).cUnitID;
-                listArmy[i][1] = Scenario.bluePlayer.combatUnits.get(i).size;
-                listArmy[i][2] = Scenario.bluePlayer.combatUnits.get(i).GetLocation().id;
-                if (Scenario.bluePlayer.combatUnits.get(i).isFleet == true)
-                    listArmy[i][3] = 1;
-                else
-                    listArmy[i][3] = 0;
-            }
-        }
+     if(curPlayer == 0)
+     {
+     listArmy = new int[Scenario.redPlayer.combatUnits.size()][4];
+     for(int i = 0; i < Scenario.redPlayer.combatUnits.size(); i++) {
+     listArmy[i][0] = Scenario.redPlayer.combatUnits.get(i).cUnitID;
+     listArmy[i][1] = Scenario.redPlayer.combatUnits.get(i).size;
+     listArmy[i][2] = Scenario.redPlayer.combatUnits.get(i).GetLocation().id;
+     if (Scenario.redPlayer.combatUnits.get(i).isFleet == true)
+     listArmy[i][3] = 1;
+     else
+     listArmy[i][3] = 0;
+     }
+     } else {
+     listArmy = new int[Scenario.bluePlayer.combatUnits.size()][4];
+     for(int i = 0; i < Scenario.bluePlayer.combatUnits.size(); i++) {
+     listArmy[i][0] = Scenario.bluePlayer.combatUnits.get(i).cUnitID;
+     listArmy[i][1] = Scenario.bluePlayer.combatUnits.get(i).size;
+     listArmy[i][2] = Scenario.bluePlayer.combatUnits.get(i).GetLocation().id;
+     if (Scenario.bluePlayer.combatUnits.get(i).isFleet == true)
+     listArmy[i][3] = 1;
+     else
+     listArmy[i][3] = 0;
+     }
+     }
 
-        return listArmy;
-    }
-    */
+     return listArmy;
+     }
+     */
 }
