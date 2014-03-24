55809d18fb8f9683f7ec3cc8542f6c8acba3d9f4
BotBen <ben.yntema@gmail.com>
2014-03-23 21:38:35 -0400

    Anti Pop-ups
    Combat is broken though, null pointers

--- War/src/CombatInstance.java
+++ War/src/CombatInstance.java
@@ -1,27 +1,28 @@
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Ben
  */
 public class CombatInstance {
-    public ArrayList<CombatUnit> redUnits, blueUnits;
-    public ArrayList<Road> redRoad, blueRoad;
-    public ArrayList<Node> redEndLocation, blueEndLocation;
-    public ArrayList<Node> redFromLocation, blueFromLocation;
-    public boolean isNode;
-    
-    public CombatInstance(ArrayList<CombatUnit> _redUnits, ArrayList<CombatUnit> _blueUnits, 
+
+    private ArrayList<CombatUnit> redUnits, blueUnits;
+    private ArrayList<Road> redRoad, blueRoad;
+    private ArrayList<Node> redEndLocation, blueEndLocation;
+    private ArrayList<Node> redFromLocation, blueFromLocation;
+    private boolean isNode;
+
+    public CombatInstance(ArrayList<CombatUnit> _redUnits, ArrayList<CombatUnit> _blueUnits,
             ArrayList<Road> _redRoad, ArrayList<Road> _blueRoad,
             ArrayList<Node> _redEndLocation, ArrayList<Node> _blueEndLocation,
-            ArrayList<Node> _redFromLocation, ArrayList<Node> _blueFromLocation, boolean _isNode){
+            ArrayList<Node> _redFromLocation, ArrayList<Node> _blueFromLocation, boolean _isNode) {
         redUnits = _redUnits;
         blueUnits = _blueUnits;
         redRoad = _redRoad;
@@ -31,5 +32,41 @@ public class CombatInstance {
         redFromLocation = _redFromLocation;
         blueFromLocation = _blueFromLocation;
         isNode = _isNode;
-    }    
+    }
+    
+    public ArrayList<CombatUnit> redUnits(){
+        return redUnits;
+    }
+    
+    public ArrayList<CombatUnit> blueUnits(){
+        return blueUnits;
+    }
+    
+    public ArrayList<Road> redRoad(){
+        return redRoad;
+    }
+    
+    public ArrayList<Road> blueRoad(){
+        return blueRoad;
+    }
+    
+    public ArrayList<Node> redEndLocation(){
+        return redEndLocation;
+    } 
+    
+    public ArrayList<Node> blueEndLocation(){
+        return blueEndLocation;
+    }
+    
+    public ArrayList<Node> redFromLocation(){
+        return redFromLocation;
+    }
+    
+    public ArrayList<Node> blueFromLocation(){
+        return blueFromLocation;
+    }
+    
+    public Boolean isNode(){
+        return isNode;
+    }
 }
--- War/src/MainMenu.java
+++ War/src/MainMenu.java
@@ -163,7 +163,7 @@ public class MainMenu extends javax.swing.JFrame {
     }//GEN-LAST:event_buttonScenario3ActionPerformed
 
     private void radioAIActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_radioAIActionPerformed
-        Global.opponent = false;
+        Global.opponent = true;
     }//GEN-LAST:event_radioAIActionPerformed
 
     private void radioPlayerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_radioPlayerActionPerformed
